b2489fac711742c81d2a026b49b7e77888673286
Abe <pasionabe@gmail.com>
2025-12-23 10:20:27 -0500

    Finished Bookbot

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from stats import get_num_words
+import sys
+from stats import *
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+relative_book_path = sys.argv[1]
+
 def get_books_text():
-    with open("books/frankenstein.txt") as f:
+    with open(relative_book_path) as f:
         file_contents = f.read()
+        print("----------- Word Count ----------")
         get_num_words(file_contents) 
+        print("--------- Character Count -------")
+        char_freq = get_char_frequency(file_contents)
+        dict_to_sorted_list(char_freq)
+
 def main():
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {relative_book_path}...")
     get_books_text()
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,27 @@ def get_num_words(text):
     word_arr = text.split()
     num_words = len(word_arr)
     print(f"Found {num_words} total words")
+
+def get_char_frequency(text):
+    char_frequency = {}
+    for char in text:
+        char = char.lower()
+        if char in char_frequency:
+            char_frequency[char] += 1
+        else:
+            char_frequency[char] = 1
+    return char_frequency
+
+def dict_to_sorted_list(items):
+    sorted_list = []
+    for k, v in items.items():
+        sorted_list.append({"char": k, "num":v})
+
+    sorted_list.sort(reverse=True, key=sort_on)
+    sorted_dict = {item["char"]: item["num"] for item in sorted_list}
+    for k, v in sorted_dict.items():
+        if k.isalpha():
+            print(f"{k}: {v}")
+    
+def sort_on(items):
+    return items["num"]
